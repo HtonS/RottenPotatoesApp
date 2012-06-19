69685f6f1635129ac045d6e77d97fdf9805c66af
HtonS <HtonS@mail>
2012-06-19 02:05:00 +0000

    Finished part 3 and fixed part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,12 +10,23 @@ class MoviesController < ApplicationController
     @checked_ratings_param = params[:ratings]
     if params[:ratings] == nil
       @checked_ratings = nil
+      if session[:ratings] != nil and session[:sort_by] != nil
+        redirect_to movies_path(:ratings => session[:ratings], :sort_by => session[:sort_by])
+      elsif session[:ratings] == nil and session[:sort_by] != nil
+        redirect_to movies_path(:sort_by => session[:sort_by])
+      elsif session[:ratings] != nil and session[:sort_by] == nil
+        redirect_to movies_path(:ratings => session[:ratings])
+      end
       @movies = Movie.find(:all, :order => params[:sort_by].to_s)
     else
       @checked_ratings = params[:ratings].keys
+      session[:ratings] = params[:ratings]
       @movies = Movie.find(:all, :order => params[:sort_by].to_s, :conditions => ["rating IN (?)", @checked_ratings])
     end
     @sort_by = params[:sort_by]
+    if @sort_by != nil
+      session[:sort_by] = params[:sort_by]
+    end
     @all_ratings = Movie.all_ratings
   end
 
